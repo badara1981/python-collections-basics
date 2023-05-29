a42d103b81fbc4298984b4fcdca80b6d2523d61a
Badara <badara.jammeh@hotmail.com>
2023-05-29 20:10:15 +0200

    added

---
+++ python-collections-basic-2-badara.py
@@ -0,0 +1,8 @@
+### Task 1 `742 Evergreen Terrace`. Print the name and the address separated by comma to the screen.
+person = {
+
+"name" : "Bart Simpson", "address"  : "742 Evergreen Terrace"
+
+}
+
+print(person["name"],person["address"])
